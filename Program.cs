e1f5befd749442da2ada529e47526ee3bdecdb4c
Kiseleva-T <107543752+Kiseleva-T@users.noreply.github.com>
2022-12-01 14:35:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Маргулис Инна Даниловна";
+            int age = 17;
+            string email = "inna2828@inbox.ru";
+            float pointsIT = 93.8f;
+            float pointsMaths = 88.78f;
+            float pointsPhysics = 99.995f;
+
+            
+            string pattern = $"ФИО: {fullName} \nВозраст: {age} \nEmail: {email} \nБаллы по информатике: {pointsIT} \nБаллы по математике: {pointsMaths} \nБаллы по физике: {pointsPhysics}";
+            Console.WriteLine(pattern,
+                              fullName,
+                              age,
+                              email,
+                              pointsIT,
+                              pointsMaths,
+                              pointsPhysics);
+            Console.ReadLine();
+
+            float sumPoints = pointsIT + pointsMaths + pointsPhysics;
+            Console.WriteLine($"Общая сумма баллов: {sumPoints}");
+            Console.ReadLine();
+
+            float meanPoints = sumPoints / 3;
+            Console.WriteLine($"Средний балл {meanPoints}");
+            Console.ReadLine();
+
+        }
+    }
+           
+            
+}
